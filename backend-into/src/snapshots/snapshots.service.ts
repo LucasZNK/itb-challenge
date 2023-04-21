279fc1f798a018e5d1d8b1981b25511325f5b7ce
Lucas Zanek <lucaszanek94@gmail.com>
2023-04-21 19:49:06 -0300

    save pair on db service

--- backend-into/src/snapshots/snapshots.service.ts
+++ backend-into/src/snapshots/snapshots.service.ts
@@ -3,55 +3,69 @@ import { SnapshotPairData } from './entities/snapshot.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Pair } from './entities/pair.entity';
+
 @Injectable()
 export class SnapshotsService {
   constructor(
     @InjectRepository(SnapshotPairData)
     private snapshotRepository: Repository<SnapshotPairData>,
     private pairRepository: Repository<Pair>,
+    private snapshotPairDataRepository: Repository<SnapshotPairData>,
   ) {}
 
   async findAll(): Promise<SnapshotPairData[]> {
     return await this.snapshotRepository.find();
   }
 
-  async savePairHourData(data) {
-    const pairHourDatas = data.data.pairHourDatas;
+  async savePairHourData(data: SnapshotPairData[]) {
+    const pairHourDatas = data;
 
-    for (const pairHourData of pairHourDatas) {
-      const pair = await this.pairRepository.findOne({
+    for (let index = 0; index < pairHourDatas.length; index++) {
+      const pairHourData = pairHourDatas[index];
+
+      let pair = await this.pairRepository.findOne({
         where: {
-          address: pairHourData.address,
+          address: pairHourData.pair.address,
         },
       });
 
-      //   // Si el par no existe, crea un nuevo registro en la tabla `pair`
-      //   if (!pair) {
-      //     pair = new Pair();
-      //     pair.token0_name = pairHourData.pair.token0.name;
-      //     pair.token1_name = pairHourData.pair.token1.name;
-      //     pair.pair_address = pairAddress;
-      //     await pairRepository.save(pair);
-      //   }
+      if (!pair) {
+        pair = new Pair();
+        pair.token0.name = pairHourData.pair.token0.name;
+        pair.token1.name = pairHourData.pair.token1.name;
+        pair.address = pairHourData.pair.address;
+        await this.pairRepository.save(pair);
+      }
 
-      //   // Crear un nuevo registro en la tabla `snapshot_pair_data`
-      //   const snapshot = new SnapshotPairData();
-      //   snapshot.pair = pair;
-      //   snapshot.hourlyVolumeToken0 = pairHourData.hourlyVolumeToken0;
-      //   snapshot.hourlyVolumeToken1 = pairHourData.hourlyVolumeToken1;
-      //   snapshot.hourlyVolumeUSD = pairHourData.hourlyVolumeUSD;
-      //   snapshot.reserve0 = pairHourData.reserve0;
-      //   snapshot.reserve1 = pairHourData.reserve1;
-      //   snapshot.reserveUSD = pairHourData.reserveUSD;
+      const snapshot = await this.createSnapshot(pairHourData, pair, index);
 
-      //   // Calcular la hora y guardarla en el registro
-      //   const now = new Date();
-      //   snapshot.timestamp = new Date(
-      //     now.getTime() -
-      //       (2 - pairHourDatas.indexOf(pairHourData)) * 60 * 60 * 1000,
-      //   );
-
-      //   await snapshotPairDataRepository.save(snapshot);
+      await this.snapshotPairDataRepository.save(snapshot);
     }
+  }
+
+  async createSnapshot(
+    pairHourData: SnapshotPairData,
+    pair: Pair,
+    index: number,
+  ) {
+    const snapshot = new SnapshotPairData();
+    snapshot.pair = pair;
+    snapshot.hourlyVolumeToken0 = pairHourData.hourlyVolumeToken0;
+    snapshot.hourlyVolumeToken1 = pairHourData.hourlyVolumeToken1;
+    snapshot.hourlyVolumeUSD = pairHourData.hourlyVolumeUSD;
+    snapshot.reserve0 = pairHourData.reserve0;
+    snapshot.reserve1 = pairHourData.reserve1;
+    snapshot.reserveUSD = pairHourData.reserveUSD;
+    const date = await this.generateTimestamp(index);
+    snapshot.timestamp = date;
+
+    return snapshot;
+  }
+
+  async generateTimestamp(hourData: number) {
+    // The graph returns the data without timestamp, so we need to generate it
+    const now = new Date();
+    const timestamp = new Date(now.getTime() - hourData * 60 * 60 * 1000);
+    return timestamp;
   }
 }
