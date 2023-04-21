74c7727e82db530298c4a957163e5c5a23f51a96
Lucas Zanek <lucaszanek94@gmail.com>
2023-04-21 17:35:39 -0300

    working on index entities

--- backend-into/src/snapshots/entities/pair.entity.ts
+++ backend-into/src/snapshots/entities/pair.entity.ts
@@ -1,17 +1,34 @@
-// pair.entity.ts
-import { ObjectType, Field } from '@nestjs/graphql';
-import { Column, Entity, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { ObjectType, Field, Int } from '@nestjs/graphql';
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  OneToMany,
+  Index,
+} from 'typeorm';
 import { SnapshotPairData } from './snapshot.entity';
 
 @ObjectType()
+class Token {
+  @Field(() => String)
+  name: string;
+
+  @Field(() => String)
+  symbol: string;
+}
+
+// pair.entity.ts
+
+@ObjectType()
 @Entity()
-class Pair {
+export class Pair {
   @PrimaryGeneratedColumn()
   @Field(() => Int)
   id: number;
 
   @Column()
   @Field(() => String)
+  @Index()
   address: string;
 
   @Column()
@@ -21,4 +38,11 @@ class Pair {
   @Column()
   @Field(() => Token)
   token1: Token;
+
+  @OneToMany(
+    () => SnapshotPairData,
+    (snapshotPairData) => snapshotPairData.pair,
+  )
+  @Field(() => [SnapshotPairData])
+  snapshotPairData: SnapshotPairData[];
 }
--- backend-into/src/snapshots/snapshots.service.ts
+++ backend-into/src/snapshots/snapshots.service.ts
@@ -2,14 +2,56 @@ import { Injectable } from '@nestjs/common';
 import { SnapshotPairData } from './entities/snapshot.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { Pair } from './entities/pair.entity';
 @Injectable()
 export class SnapshotsService {
   constructor(
     @InjectRepository(SnapshotPairData)
     private snapshotRepository: Repository<SnapshotPairData>,
+    private pairRepository: Repository<Pair>,
   ) {}
 
   async findAll(): Promise<SnapshotPairData[]> {
     return await this.snapshotRepository.find();
+  }
+
+  async savePairHourData(data) {
+    const pairHourDatas = data.data.pairHourDatas;
+
+    for (const pairHourData of pairHourDatas) {
+      const pair = await this.pairRepository.findOne({
+        where: {
+          address: pairHourData.address,
+        },
+      });
+
+      //   // Si el par no existe, crea un nuevo registro en la tabla `pair`
+      //   if (!pair) {
+      //     pair = new Pair();
+      //     pair.token0_name = pairHourData.pair.token0.name;
+      //     pair.token1_name = pairHourData.pair.token1.name;
+      //     pair.pair_address = pairAddress;
+      //     await pairRepository.save(pair);
+      //   }
+
+      //   // Crear un nuevo registro en la tabla `snapshot_pair_data`
+      //   const snapshot = new SnapshotPairData();
+      //   snapshot.pair = pair;
+      //   snapshot.hourlyVolumeToken0 = pairHourData.hourlyVolumeToken0;
+      //   snapshot.hourlyVolumeToken1 = pairHourData.hourlyVolumeToken1;
+      //   snapshot.hourlyVolumeUSD = pairHourData.hourlyVolumeUSD;
+      //   snapshot.reserve0 = pairHourData.reserve0;
+      //   snapshot.reserve1 = pairHourData.reserve1;
+      //   snapshot.reserveUSD = pairHourData.reserveUSD;
+
+      //   // Calcular la hora y guardarla en el registro
+      //   const now = new Date();
+      //   snapshot.timestamp = new Date(
+      //     now.getTime() -
+      //       (2 - pairHourDatas.indexOf(pairHourData)) * 60 * 60 * 1000,
+      //   );
+
+      //   await snapshotPairDataRepository.save(snapshot);
+    }
   }
 }
