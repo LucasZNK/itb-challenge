bcf9407957b8c2e86c4382594cf0eb173dbe27ca
Lucas Zanek <lucaszanek94@gmail.com>
2023-04-24 19:42:52 -0300

    added docker and compose. Working the entire project with docker compose up

--- backend-into/src/app.module.ts
+++ backend-into/src/app.module.ts
@@ -7,20 +7,21 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { ScheduleModule } from '@nestjs/schedule';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
     TypeOrmModule.forRoot({
-      // move to env variables
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '1234',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
       database: 'snapshots',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
